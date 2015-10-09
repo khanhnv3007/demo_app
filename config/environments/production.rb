04b12f9c5664570d178381951db5eac4719d1c37
khanhnv3007 <khanhnv3007@gmail.com>
2015-10-09 10:25:54 +0700

    change the host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'khanhnv3007-sample-app.herokuapp.com'
+  host = 'khanhnv3007-demo-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
